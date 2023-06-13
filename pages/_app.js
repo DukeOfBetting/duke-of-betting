1c7eede3ca8b5e006aebf42aa013c1c4e79ca8b4
Gurbinder <gurbindersingh8727@gmail.com>
2023-06-13 23:32:24 +0530

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="bg-gradient-to-bl from-[#1d3039] to-[#172329] h-screen w-screen text-[#dfe0db] overflow-hidden">
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
